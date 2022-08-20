50d7537da4790ca60324c0c56560315be7b26c85
kecy <kec.bigmt@gmail.com>
2022-08-20 23:32:39 +0900

    add dojoExample1 source code

--- dojoExample1/src/ofApp.cpp
+++ dojoExample1/src/ofApp.cpp
@@ -2,17 +2,26 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+    ofSetWindowShape(600, 400);
+    ofSetFrameRate(30);
+    ofSetBackgroundAuto(false);
+    ofSetBackgroundColor(255, 255, 255);
+    ofSetCircleResolution(64);
 
+    rad = 0.0;
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    rad += 0.05;
+    posX = 300 + 150 * cos(rad * 5);
+    posY = 200 + 150 * sin(rad * 6);
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    ofSetColor(215, 19, 69);
+    ofDrawCircle(posX, posY, 20);
 }
 
 //--------------------------------------------------------------
